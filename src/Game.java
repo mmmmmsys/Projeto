bc21cfe2205026dae40fdac8dc794aa1fa9dc54e
Mariana <mariana.melo@school.mindera.com>
2025-01-10 15:07:06 +0000

    updated

---
+++ src/Game.java
@@ -0,0 +1,6 @@
+public class Game {
+
+    public Game(Player player1, Player player2) {
+    }
+    int[][]board = new int[8][8];
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Your name?");
+        String name = input.next();
+        System.out.print("Your checker color?");
+        String color  = input.next();
     }
 }
